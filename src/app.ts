3303863955e36ba4950a4f0381fcf851f336f80a
alberdg811 <albertogodar81@gmail.com>
2021-01-30 12:57:34 +0100

    Added Get Teams route to express app

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,11 @@ import { json } from 'body-parser';
 import helmet from 'helmet';
 import { errorHandler } from './middlewares/error-handler';
 import { NotFoundError } from './errors/not-found-error';
-
+import { indexTeamsRouter } from './routes/teams';
 const app = express();
 app.use(json());
 app.use(helmet());
+app.use(indexTeamsRouter);
 
 app.all('*', (req: Request, res: Response) => {
   throw new NotFoundError();
