0f4f8d0f40b7557936e02eb5d749ecd315b05ae5
alberdg811 <albertogodar81@gmail.com>
2021-01-30 13:42:43 +0100

    Created constants file

---
+++ src/constants/index.ts
@@ -0,0 +1,5 @@
+import { Team } from '../models/team';
+export const WATFORD_ITEM: Team = {
+  'name': 'Watford',
+  'img': 'https://s3-eu-west-1.amazonaws.com/inconbucket/images/entities/original/638.jpg'
+};
