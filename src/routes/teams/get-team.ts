a722aad719ecec2bd499b0b465c30cec4ca0080c
alberdg811 <albertogodar81@gmail.com>
2021-01-30 13:44:01 +0100

    Added get team router

---
+++ src/routes/teams/get-team.ts
@@ -0,0 +1,14 @@
+import { Request, Response, Router } from 'express';
+import { Team } from '../../models/team';
+import { teamsService } from '../../services/teams';
+import { NotFoundError } from '../../errors/not-found-error';
+const router = Router();
+
+router.get('/teams/:name',async (req: Request, res: Response) => {
+  const team: Team = await teamsService.findTeam(req.params.name);
+  if (!team) {
+    throw new NotFoundError();
+  }
+  res.status(200).send(team);
+});
+export { router as getTeamRouter };
