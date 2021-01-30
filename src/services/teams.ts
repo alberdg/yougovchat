87dce6ad71e954e10e854c04e95701e8d5150813
alberdg811 <albertogodar81@gmail.com>
2021-01-30 18:01:33 +0100

    Refactored the service as it is far more efficient to use a Map to hold the teams. Added updateTeam method

--- src/services/teams.ts
+++ src/services/teams.ts
@@ -4,21 +4,20 @@ import { CreateTeamResponse } from '../models/create-team-response';
 const TEAMS_GIST_URL: string = 'https://gist.githubusercontent.com/joaofs/a6b80ce482de2f3846a00e72c0497a35/raw/07b62f4971dcdb11895a3138e6f0a91aeeea1036/football.json';
 
 class TeamsService {
-  private _teams: Team[];
+  private _teams: Map<string,Team>;
 
   constructor () {
-    this._teams = null!;
+    this._teams = new Map();
   }
 
-
   async fetchTeams(): Promise<Team[]> {
-    if (!Array.isArray(this._teams)) {
+    if (this._teams.size === 0) {
       const response: AxiosResponse<Team[]> = await axios(TEAMS_GIST_URL);
       if (response && response.data) {
-        this._teams = response.data;
+        response.data.map(team => this._teams.set(team.name.toLowerCase(), team));
       }
     }
-    return this._teams
+    return Array.from(this._teams.values());
   }
 
   async findTeamByName (team: string): Promise<Team> {
@@ -27,22 +26,34 @@ class TeamsService {
     }
     await this.fetchTeams();
     const lowerCaseTeam: string = team.toLowerCase();
-    return this._teams.find((team: Team) => team.name.toLowerCase() === lowerCaseTeam)!;
+    return this._teams.get(lowerCaseTeam)!;
   }
 
   async createTeam (team: Team): Promise<CreateTeamResponse> {
     if (!team) {
       return null!;
     }
-    const teamFound: Team = await this.findTeamByName(team.name);
+    const { name } = team;
+    const teamFound: Team = await this.findTeamByName(name);
     if (teamFound) {
       return { status: 200, team: teamFound };
     }
-    this._teams.push(team);
+    this._teams.set(name.toLowerCase(), team);
     return { status: 201, team };
   }
 
-
+  async updateTeam (team: Team) : Promise<Team> {
+    if (!team) {
+      return null!;
+    }
+    const { name } = team;
+    const teamFound: Team = await this.findTeamByName(name);
+    if (teamFound) {
+      this._teams.set(name.toLowerCase(), team);
+      return team;
+    }
+    return null!;
+  }
 }
 
 export const teamsService: TeamsService = new TeamsService();
