74c4929b2a8f413e7a61c11eda09b1ee4d9c1b16
alberdg811 <albertogodar81@gmail.com>
2021-01-30 17:12:33 +0100

    NOTTINGHAM_FOREST_ITEM added to constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,10 @@
 import { Team } from '../models/team';
 export const WATFORD_ITEM: Team = {
-  'name': 'Watford',
-  'img': 'https://s3-eu-west-1.amazonaws.com/inconbucket/images/entities/original/638.jpg'
+  name: 'Watford',
+  img: 'https://s3-eu-west-1.amazonaws.com/inconbucket/images/entities/original/638.jpg'
+};
+
+export const NOTTINGHAM_FOREST_ITEM: Team = {
+  name: 'Nottingham Forest',
+  img: 'https://ssl.gstatic.com/onebox/media/sports/logos/Zr6FbE-8pDH7UBpWCO8U9A_96x96.png'
 };
