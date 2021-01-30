829018589e67007edbdeff1a598b55ca2f98f77c
alberdg811 <albertogodar81@gmail.com>
2021-01-30 13:43:04 +0100

    Added find team service test

--- src/services/__test__/teams.test.ts
+++ src/services/__test__/teams.test.ts
@@ -1,8 +1,18 @@
 import { Team } from '../../models/team';
 import { teamsService } from '../teams';
+import { WATFORD_ITEM } from '../../constants';
 
 it('Retrieves a list of teams', async () => {
   const teams: Team[] = await teamsService.fetchTeams();
   expect(Array.isArray(teams)).toBeTruthy();
   expect(teams.length).toBe(20);
 });
+
+
+it('Retrieves a team with case sensitive', async () => {
+  const team: Team = await teamsService.findTeam('watford');
+  const { name, img } = WATFORD_ITEM;
+  expect(team).not.toBeNull();
+  expect(team.name).toBe(name);
+  expect(team.img).toBe(img);
+})
