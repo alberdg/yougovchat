3788aca723f219613b9aa4a8634d7aea17523813
alberdg811 <albertogodar81@gmail.com>
2021-01-30 17:29:11 +0100

    Create team service tests

--- src/services/__test__/teams.test.ts
+++ src/services/__test__/teams.test.ts
@@ -1,4 +1,5 @@
 import { Team } from '../../models/team';
+import { CreateTeamResponse } from '../../models/create-team-response';
 import { teamsService } from '../teams';
 import { WATFORD_ITEM, NOTTINGHAM_FOREST_ITEM } from '../../constants';
 
@@ -17,9 +18,21 @@ it('Retrieves a team by its name', async () => {
 });
 
 it('Creates a team if it does not exist', async () => {
-  const team: Team = await teamsService.createTeam(NOTTINGHAM_FOREST_ITEM);
+  const teamResponse: CreateTeamResponse = await teamsService.createTeam(NOTTINGHAM_FOREST_ITEM);
   const { name, img } = NOTTINGHAM_FOREST_ITEM;
-  expect(team).not.toBeNull();
-  expect(team.name).toBe(name);
-  expect(team.img).toBe(img);
+  expect(teamResponse).not.toBeNull();
+  expect(teamResponse.status).toBe(201);
+  expect(teamResponse.team).not.toBeNull();
+  expect(teamResponse.team.name).toBe(name);
+  expect(teamResponse.team.img).toBe(img);
+});
+
+it('Tries to create an existing team', async () => {
+  const teamResponse: CreateTeamResponse = await teamsService.createTeam(WATFORD_ITEM);
+  const { name, img } = WATFORD_ITEM;
+  expect(teamResponse).not.toBeNull();
+  expect(teamResponse.status).toBe(200);
+  expect(teamResponse.team).not.toBeNull();
+  expect(teamResponse.team.name).toBe(name);
+  expect(teamResponse.team.img).toBe(img);
 });
