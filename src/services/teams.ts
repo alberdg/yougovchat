21ba6a1c28867595e37a0c4b1290c383cd631386
alberdg811 <albertogodar81@gmail.com>
2021-01-30 17:29:34 +0100

    Create team service implementation

--- src/services/teams.ts
+++ src/services/teams.ts
@@ -1,5 +1,6 @@
 import axios, { AxiosResponse } from 'axios';
 import { Team } from '../models/team';
+import { CreateTeamResponse } from '../models/create-team-response';
 const TEAMS_GIST_URL: string = 'https://gist.githubusercontent.com/joaofs/a6b80ce482de2f3846a00e72c0497a35/raw/07b62f4971dcdb11895a3138e6f0a91aeeea1036/football.json';
 
 class TeamsService {
@@ -29,16 +30,16 @@ class TeamsService {
     return this._teams.find((team: Team) => team.name.toLowerCase() === lowerCaseTeam)!;
   }
 
-  async createTeam (team: Team): Promise<Team> {
+  async createTeam (team: Team): Promise<CreateTeamResponse> {
     if (!team) {
       return null!;
     }
     const teamFound: Team = await this.findTeamByName(team.name);
     if (teamFound) {
-      return teamFound;
+      return { status: 200, team: teamFound };
     }
     this._teams.push(team);
-    return team;
+    return { status: 201, team };
   }
 
 
